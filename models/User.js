95e31c59f26030468e51ce2d84bcf4920d08d6c0
SuperiorGitTele <semovita56@gmail.com>
2024-11-12 10:37:59 +0100

    Login Auth fix

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({ // Change 'mongoose.connect' to 'mongoose.Schema'
+const userSchema = new mongoose.Schema({ 
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
